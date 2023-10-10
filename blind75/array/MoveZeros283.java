01243485a1f79c2ef989b0c3ac9410a8dd95b4e0
shakibaenur <shakibaenur2612@gmail.com>
2023-10-10 22:22:42 +0600

    Leetcode 283 solved using 2 pointer approach

---
+++ blind75/array/MoveZeros283.java
@@ -0,0 +1,16 @@
+package blind75.array;
+
+public class MoveZeros283 {
+    class Solution {
+        public void moveZeroes(int[] nums) {
+            int j=0;
+            for(int i=0;i<nums.length;i++){
+                if(nums[i]!=0){
+                    nums[j++]=nums[i];
+                }
+
+            }
+            while(j<nums.length)nums[j++]=0;
+        }
+    }
+}
