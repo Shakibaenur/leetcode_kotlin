4487810ce4cdc8c68454910842d722e312b08c15
shakibaenur <shakibaenur2612@gmail.com>
2023-10-24 21:07:14 +0600

    Two sum solution with time limit exit

--- blind75/array/TwoSum.java
+++ blind75/array/TwoSum.java
@@ -1,5 +1,6 @@
 package blind75.array;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -18,5 +19,33 @@ class Solution {
         }
 
         throw new IllegalArgumentException("No two sum solution");
+    }
+}
+
+
+
+class Solution1 {
+    //Time Limit Exit
+    public int[] twoSum(int[] nums, int target) {
+
+        int[] ans = new int[2];
+        ans[0]=-1;
+        ans[1]=-1;
+        int n=nums.length-1;
+        Arrays.sort(nums);
+        int left=0;
+        int right=n-1;
+        while(left<right){
+            int sum=nums[left]+nums[right];
+            if(sum==target){
+                ans[0]=nums[left];
+                ans[1]=nums[right];
+            }else if(sum<target){
+                left++;
+            }else{
+                right--;
+            }
+        }
+        return ans;
     }
 }
