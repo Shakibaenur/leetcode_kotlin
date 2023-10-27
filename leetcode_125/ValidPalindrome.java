ff363746ed84f10f48badbd57889daf2fa441f4a
shakibaenur <shakibaenur2612@gmail.com>
2023-10-27 22:09:34 +0600

    Valid Palindrome leet code 125

---
+++ leetcode_125/ValidPalindrome.java
@@ -0,0 +1,29 @@
+package leetcode_125;
+
+public class ValidPalindrome {
+
+    class Solution {
+        public boolean isPalindrome(String s) {
+            if(s.isEmpty())
+                return true;
+            char[] arr=s.toCharArray();
+            int left=0;
+            int right=arr.length-1;
+            while(left<=right){
+                if(!Character.isLetterOrDigit(left)){
+                    left++;
+                }else if(!Character.isLetterOrDigit(right)){
+                    right--;
+                }else{
+                    if(Character.toLowerCase(arr[left])!=Character.toLowerCase(arr[right])){
+                        return false;
+                    }
+
+                    left++;
+                    right--;
+                }
+            }
+            return true;
+        }
+    }
+}
