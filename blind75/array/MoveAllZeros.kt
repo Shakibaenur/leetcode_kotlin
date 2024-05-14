464d843912553f3332032144c87ebf50b4028dfa
shakibaenur <shakibaenur2612@gmail.com>
2024-05-14 23:11:03 +0600

    solution of 283. Move Zeroes

---
+++ blind75/array/MoveAllZeros.kt
@@ -0,0 +1,27 @@
+package blind75.array
+
+class MoveAllZeros {
+    class Solution {
+        class Solution {
+            fun moveZeroes(nums: IntArray): Unit {
+                var n=nums.size
+                var left=0
+                var right=0
+                if(n==1){
+                    return
+                }
+                while(right<n){
+                    if(nums[right]!=0){
+                        var temp=nums[left]
+                        nums[left]=nums[right]
+                        nums[right]=temp
+                        left++
+                    }
+                    right++
+                }
+            }
+
+        }
+//T.C: O(n)
+//S.C: O(1)
+}
